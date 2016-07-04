6952f07ced07f24a20deab086e3b9b01300ca985
liusongWtu <997896026@qq.com>
2016-07-04 16:31:36 +0800

    create project

---
+++ MyPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPhoto
+//
+//  Created by song on 16/7/1.
+//  Copyright © 2016年 timelink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
