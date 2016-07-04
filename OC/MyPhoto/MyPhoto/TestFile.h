13caf4778f9ae96c0a2a94cc41f98975b047e730
liusongWtu <997896026@qq.com>
2016-07-05 00:34:26 +0800

    add testfile

---
+++ OC/MyPhoto/MyPhoto/TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  MyPhoto
+//
+//  Created by song on 16/7/5.
+//  Copyright © 2016年 timelink. All rights reserved.
+//
+
+#ifndef TestFile_h
+#define TestFile_h
+
+
+#endif /* TestFile_h */
