b8f461ace1a5a3c2b6b69050705e11584246f125
liusongWtu <997896026@qq.com>
2016-07-05 01:16:36 +0800

    test2

---
+++ OC/MyPhoto/MyPhoto/test2.h
@@ -0,0 +1,13 @@
+//
+//  test2.h
+//  MyPhoto
+//
+//  Created by song on 16/7/5.
+//  Copyright © 2016年 timelink. All rights reserved.
+//
+
+#ifndef test2_h
+#define test2_h
+
+
+#endif /* test2_h */
