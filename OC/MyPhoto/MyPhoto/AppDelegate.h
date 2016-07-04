1276af6c26846650744882a71bf3415d022b4409
liusongWtu <997896026@qq.com>
2016-07-04 23:33:56 +0800

    change directory

---
+++ OC/MyPhoto/MyPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPhoto
+//
+//  Created by song on 16/7/1.
+//  Copyright © 2016年 timelink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
